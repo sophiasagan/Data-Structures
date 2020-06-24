be187133be91051ef2d2e3747dbc1def5f837538
Sophia Sagan <sophiabraddock82@gmail>
2020-06-23 20:54:30 -0400

    day 2 done

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 """
 Binary search trees are a data structure that enforce an ordering over 
 the data they store. That ordering in turn makes it a lot more efficient 
@@ -79,22 +82,85 @@ class BSTNode:
         if self.right:
             self.right.for_each(fn)
 
+    def iter_depth_first_for_each(self, fn):
+        # with depth-first traversal, there's a certain order to when we visit nodes
+        # what's the order?
+        # init a stack to keep track of the order of nodes we visited
+        stack = []
+        # add the first node to our stack
+        stack.append(self)
+        # continue traversing until our stack is empty
+        while len(stack) > 0:
+            # pop off the stack
+            current_node = stack.pop
+            # add its children to the stack
+            # add the right child first and left child second
+            # to ensure that lef is popped off the stack first
+            if current_node.right:
+                stack.append(current_node.right)
+            if current_node.left:
+                stack.append(current_node.left)
+            # call the fn function on self.value
+            fn(self.value)
+
+    def iter_breadth_first_for_each(self, fn):
+        # breadth first traversal follow FIFO ordering of its nodes
+        # init a deque
+        q = deque()
+        # add the first node to our q
+        q.append(self)
+
+        while len(q) > 0:
+            current_node = q.popleft()
+            if current_node.left:
+                q.append(current_node.left)
+            if current_node.right:
+                q.append(current_node.right)
+            fn(self.value)
+            
+
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        if node is not None:
+            self.in_order_print(node.left)
+            print(node.value)
+            self.in_order_print(node.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        q = deque()
+        q.append(self)
+
+        while q:
+            node = q.popleft()
+            if node.left is not None:
+                q.append(node.left)
+            if node.right is not None:
+                q.append(node.right)
+            print(node.value)
+
+        return
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        stack = []
+        stack.append(node)
+
+        while stack:
+            node = stack.pop()
+            print(node.value)
+
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return
 
     # Stretch Goals -------------------------
     # Note: Research may be required
